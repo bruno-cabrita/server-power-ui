cc173406fce7ff7d2a60081934637ff43270538a
Bruno Cabrita <b.cabrita@gmail.com>
2025-05-24 17:59:14 +0100

    minimal updates

--- src/layouts/Base.tsx
+++ src/layouts/Base.tsx
@@ -6,6 +6,7 @@ const Layout: FC = ({ children }) => {
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
         <meta name="theme-color" content="#030712" />
         <title>Server Power UI</title>
         <link rel="stylesheet" href="css/styles.css" />
