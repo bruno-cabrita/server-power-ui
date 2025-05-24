ad729fff524d3843fe827352f8595cb24d394806
Bruno Cabrita <b.cabrita@gmail.com>
2025-05-24 17:47:40 +0100

    favicon added

--- src/layouts/Base.tsx
+++ src/layouts/Base.tsx
@@ -8,8 +8,9 @@ const Layout: FC = ({ children }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="theme-color" content="#030712" />
         <title>Server Power UI</title>
-        <link rel="stylesheet" href="/css/styles.css" />
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="stylesheet" href="css/styles.css" />
+        <link rel="manifest" href="manifest.json" />
+        <link rel="shortcut icon" href="favicon.svg" type="image/svg"></link>
       </head>
       <body class="min-h-100 bg-gray-950 text-gray-500 flex flex-col items-stretch">
         {children}
